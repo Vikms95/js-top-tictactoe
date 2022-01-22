2b99540b4cd890921a3b8bae40a667b4e7fc85ae
Vikms <vms1995@hotmail.com>
2022-01-22 21:08:38 +0100

    Add checking of winner logic

--- notes.md
+++ notes.md
@@ -18,4 +18,10 @@ Components: MODULE >
               renderGameBoard(private)
 
 <!-- - Gameboard is an array with 9 indexes, each one of them belong to a square in the display -->
-- Switch between player 1 and player 2 between turns
+<!-- - Switch between player 1 and player 2 between turns -->
+<!-- - Store values of text content inside the array
+    :assign an id with a number to every square
+    :use it as an index indicator to later push the value inside into the array -->
+
+- Check if there is any winner after event listened
+    :iterate every array of winScenarios object and check if all three numbers of the array exist in your playerMoves
--- script.js
+++ script.js
@@ -1,46 +1,82 @@
-const playerFactory = (name,mark) => {
-    
-    let _score = 0;
-    return {mark};
+const playerFactory = (name,mark,color) => {
+    let markedPositions = []
+    return {name,mark,color, markedPositions};
 }
 
 const boardModule = (() => {
-    
-    let Gameboard  = {gameBoard:["","","","","","","","",""]};
-
     const gameboardDivReference = document.querySelector('#gameboard');
+    let Gameboard = {gameBoard:['','','','','','','','','']};
     let playerTurn = undefined;   
 
     const renderGameBoard = (() => {
         const gameBoard = Gameboard.gameBoard;
-        const arrayLength = gameBoard.length;
-        gameBoard.forEach(index => {
+        for (let i = 0; i < gameBoard.length; i++) {
             let element = document.createElement('div');
+            element.id = i;
             element.className = 'board-square'
-            element.textContent = index;
             gameboardDivReference.appendChild(element);
-        })
+        }
     })();
 
     const addListenerGameBoardSquares = (() => {
         const boardSquareDivReference = document.querySelectorAll('.board-square');
         boardSquareDivReference.forEach(element => {
             element.addEventListener('click', (event) => {
-                if(event.target.textContent !== '') return;
-                if(playerTurn === undefined || playerTurn === player2){
-                    playerTurn = player1;
-                }else{
-                    playerTurn = player2;
-                }
-                event.target.textContent = playerTurn.mark;
-                
+                drawMarkOnGameboard(event);
+                // console.log(Gameboard.gameBoard)
             })
         });
     })();
 
+    const drawMarkOnGameboard = (event) => {
+        if(event.target.textContent !== '') return;
+        if(playerTurn === player2 || playerTurn === undefined){
+            playerTurn = player1;
+            event.target.style.color = playerTurn.color;
+        }else{
+            playerTurn = player2;
+            event.target.style.color = playerTurn.color;
+        }
+        let playerMoves = playerTurn.markedPositions;
+        let playerName = playerTurn.name;
+        event.target.textContent = playerTurn.mark;
+        Gameboard.gameBoard.splice(parseInt(event.target.id) , 1, playerTurn.mark);
+        playerMoves.push(event.target.id)
+        Object.values(gameModule.winScenarios).forEach((element) =>{
+            let amountFound = 0;
+            element.forEach((number) =>{
+                if(playerMoves.includes(number)){
+                    amountFound++;
+                    console.log(amountFound)
+                }
+            })
+            if(amountFound === 3){
+                gameModule.congratulateWin(playerTurn);
+            }
+        });
+    }
+    return{Gameboard};
 })();
 
-let player1 = playerFactory('Victor','X');
-let player2 = playerFactory('Olga','O');
+const gameModule = (() =>{
+    const winScenarios = {
+        horizontalWin1: ['0','1','2'],
+        horizontalWin2: ['3','4','5'],
+        horizontalWin3: ['6','7','8'],
+        verticalWin1:['0','3','6'],
+        verticalWin2:['1','4','7'],
+        verticalWin3:['2','5','8'],
+        diagonalWin1:['0','4','8'],
+        diagonalWin2:['2','4','6']
+    }
+
+    const congratulateWin = (playerName) =>{
+        console.log("The winner is " + playerName.name);
+    }
+    return {congratulateWin,winScenarios};
+})();
+
+let player1 = playerFactory('Victor','X','red');
+let player2 = playerFactory('Olga','O','blue');
 
 
