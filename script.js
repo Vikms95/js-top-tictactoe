08996bf3aaf96b14658eae46ebf490bb4999f668
Vikms <vms1995@hotmail.com>
2022-01-23 14:50:27 +0100

    Add getters and setters to keep some variables and functions private

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const playerFactory = (name,mark,color) => {
-    let markedPositions = []
-    return {name, mark, color, markedPositions};
+    let _markedPositions = []
+
+    const getMarkedPositions = () => {
+        return _markedPositions;
+    }
+    const resetMarkedPositions = () => {
+        _markedPositions = []
+    }
+
+    return {name, mark, color, getMarkedPositions,resetMarkedPositions};
 }
 
 const boardModule = (() => {
-    let boardSquareDivReference = document.querySelectorAll('.board-square');
-    let playerOnTurn = undefined; 
-    let Gameboard = {
-        gameBoard:['','','','','','','','','']
-    };
-      
+    let _playerOnTurn = undefined; 
+    let _gameBoard = ['','','','','','','','',''];
+    const getBoard = () => {
+        return _gameBoard; 
+    }
+
+    const getPlayerOnTurn = () => {
+        return _playerOnTurn;
+    }
+
+    const setPlayerOnTurn = (playerToSet) => {
+        _playerOnTurn = playerToSet;
+    }
+
     const renderGameBoard = (() => {
         const gameboardDivReference = document.querySelector('#gameboard');
-        for (let i = 0; i < Gameboard.gameBoard.length; i++) {
+        for (let i = 0; i < getBoard().length; i++) {
             let element = document.createElement('div');
             element.id = i;
             element.className = 'board-square'
@@ -20,30 +36,36 @@ const boardModule = (() => {
         }
     })();
 
-    const addListenerGameBoardSquares = (() => {
-        let boardSquareDivReference = document.querySelectorAll('.board-square');
+    const addListenerGameBoardDiv = (() => {
+        const boardSquareDivReference = document.querySelectorAll('.board-square');
         boardSquareDivReference.forEach(element => {
             element.addEventListener('click', (event) => {
-               let playerOnTurn = drawMarkOnGameboard(event);
-               if(playerOnTurn === null) return;
-               gameFlowModule.checkForWinnerOrTie(playerOnTurn);
+               drawMarkOnGameboard(event);
+               gameFlowModule.checkForWinnerOrTie(getPlayerOnTurn());
             })
+        });
+    })();
+
+    const addListenerNewGameButton = (() => {
+        const newGameButtonReference = document.querySelector('.start-game')
+        newGameButtonReference.addEventListener('click', () =>{
+            resetBoard();
         });
     })();
 
     const drawMarkOnGameboard = (event) => {
         if(event.target.textContent !== '') return null;
-        if(playerOnTurn === player2 || playerOnTurn === undefined){
-            playerOnTurn = player1;
-            event.target.style.color = playerOnTurn.color;
+        if(getPlayerOnTurn() === player2 || getPlayerOnTurn() === undefined){
+            setPlayerOnTurn(player1);
+            event.target.style.color = getPlayerOnTurn().color;
         }else{
-            playerOnTurn = player2;
-            event.target.style.color = playerOnTurn.color;
+            setPlayerOnTurn(player2);
+            event.target.style.color = getPlayerOnTurn().color;
         }
-        event.target.textContent = playerOnTurn.mark;
-        playerOnTurn.markedPositions.push(event.target.id);
-        Gameboard.gameBoard.splice(event.target.id , 1, playerOnTurn.mark);
-        return playerOnTurn;
+        event.target.textContent = getPlayerOnTurn().mark;
+        
+        getPlayerOnTurn().getMarkedPositions().push(event.target.id);
+        getBoard().splice(event.target.id , 1, getPlayerOnTurn().mark);
     };
 
     const resetBoard = () =>{
@@ -54,22 +76,28 @@ const boardModule = (() => {
         })
 
         //Clear gameboard array
-        for (let i = 0; i < Gameboard.gameBoard.length; i++) {
-            Gameboard.gameBoard[i] = '';
+        for (let i = 0; i < getBoard().length; i++) {
+            getBoard()[i] = '';
         }
 
         //Clear player marked positions
-        player1.markedPositions = [];
-        player2.markedPositions = [];
-    }
+        player1.resetMarkedPositions();
+        player2.resetMarkedPositions();
 
-    return{Gameboard,playerOnTurn,resetBoard};
+        //Resets which player has to play to it's initial state
+        setPlayerOnTurn(undefined);
+    };
 
+    return{getBoard,
+        resetBoard,
+        getPlayerOnTurn,
+        setPlayerOnTurn
+    };
 })();
 
 const gameFlowModule = (() =>{
     
-    const winScenarios = {
+    const _winScenarios = {
         horizontalWin1: ['0','1','2'],
         horizontalWin2: ['3','4','5'],
         horizontalWin3: ['6','7','8'],
@@ -80,16 +108,20 @@ const gameFlowModule = (() =>{
         diagonalWin2:['2','4','6']
     }
 
-    const checkForWinnerOrTie = (playerOnTurn) =>{
-        if(boardModule.Gameboard.gameBoard.includes('')){
-            Object.values(winScenarios).forEach((element) =>{
+    const getWinScenarios = () =>{
+        return _winScenarios;
+    }
+
+    const checkForWinnerOrTie = (getPlayerOnTurn) =>{
+        if(boardModule.getBoard().includes('')){
+            Object.values(getWinScenarios()).forEach((element) =>{
                 let amountFound = 0;
                 element.forEach((number) =>{
-                    if(playerOnTurn.markedPositions.includes(number)){
+                    if(getPlayerOnTurn.getMarkedPositions().includes(number)){
                         amountFound++;
                     }
                     if(amountFound === 3){
-                        congratulateWin(playerOnTurn);
+                        congratulateWin(boardModule.getPlayerOnTurn());
                         boardModule.resetBoard();
                     }
                 })
@@ -99,15 +131,15 @@ const gameFlowModule = (() =>{
         }
     };
 
-    const congratulateWin = (playerOnTurn) =>{
+    const congratulateWin = (getPlayerOnTurn) =>{
         const messageDivReference = document.querySelector('.message-board');
-        messageDivReference.textContent = "The winner is " + playerOnTurn.name;
+        messageDivReference.textContent = "The winner is " + getPlayerOnTurn.name;
     }
 
-    return {congratulateWin,winScenarios,checkForWinnerOrTie};
+    return {congratulateWin,
+            checkForWinnerOrTie
+    };
 })();
 
 let player1 = playerFactory('Victor','X','red');
 let player2 = playerFactory('Olga','O','blue');
-
-
